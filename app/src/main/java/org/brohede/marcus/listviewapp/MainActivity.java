1c75d35ff0542929d7e369e3e46a8fe4b158ab60
a17ludca <zhendiac@gmail.com>
2018-04-11 20:21:53 +0200

    this is final

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -1,15 +1,22 @@
 package org.brohede.marcus.listviewapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private String[] mountainNames = {"Matterhorn","Mont Blanc","Denali"};
     private String[] mountainLocations = {"Alps","Alps","Alaska"};
-    private int[] mountainHeights ={4478,4808,6190};
+    private String[] mountainHeights ={"14692ft","15777ft","20310ft"};
     // Create ArrayLists from the raw data above and use these lists when populating your ListView.
 
 
@@ -18,6 +25,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+        final List<String> mNames = new ArrayList<String>(Arrays.asList(mountainNames));
+        final List<String> mLoc = new ArrayList<String>(Arrays.asList(mountainLocations));
+        final List<String> mHeight = new ArrayList<String>(Arrays.asList(mountainHeights));
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),R.layout.list_item_textview,
+                R.id.my_item_textview,mNames);
+
+        ListView myListView = (ListView)findViewById(R.id.my_listview);
+        myListView.setAdapter(adapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int item, long id) {
+                Context context = getApplicationContext();
+                CharSequence text = mLoc.get(item)+" "+mHeight.get(item);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
         // The onCreate method is run when the app is created.
         // Before you can implement this you need to create the layout xml files that
         // will hold/show your data created here. You need three create things:
